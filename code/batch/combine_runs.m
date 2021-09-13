51e395862f6fffd76aa8f6ca31fe03122f313121
Chase Abram <abram@uchicago.edu>
2021-09-13 09:07:19 -0500

    news for new baseline, including fixes for hjb_divisor, hopefully

--- code/batch/combine_runs.m
+++ code/batch/combine_runs.m
@@ -5,8 +5,8 @@ cd('/Users/chaseabram/UChiGit/Continuous_Time_HA');
 % Change file path when running locally
 local_run = true;
 % local_path = 'output/server-all-08-19-2021-07:32:10';
-local_path = 'output/server-all-09-10-2021-08:33:19';
-name_ext = 'new_2A_base_rob2';
+local_path = 'output/server-all-09-13-2021-07:40:54';
+name_ext = 'new_2A_base_rob3';
 disp(name_ext)
 
 [~, currdir] = fileparts(pwd());
