ad5a538979dde392e35a1ed73f1e6d1a17c71bc5
Chase Abram <abram@uchicago.edu>
2022-03-23 08:33:08 -0500

    1A quads

--- code/+params/overall_htm_target.m
+++ code/+params/overall_htm_target.m
@@ -513,47 +513,47 @@ function [outparams, n] = overall_htm_target(param_opts)
             quad_bs = linspace(0.5, 50.0, 20);
             b_bds = [0.1, 50];
             rhos = [0 0.004 0.008 params{1}.rho 0.02 0.05];
-            % 1A (with 2A r_b)
-            for quad_b = quad_bs
-                for rho = rhos
-                    ii = ii + 1;
-                    params = [params {calibrations{1}}];
-                    params{ii} = params{1};
-                    params{ii}.calibration_vars = {'quad_b'};
-                    params{ii}.calibration_bounds = {b_bds};
-                    params{ii}.calibration_stats = {'totw'};
-                    params{ii}.calibration_targets = [scf.mean_totw];
-                    params{ii}.calibration_scales = [1];
-                    params{ii}.rho = rho;
-                    params{ii}.na = 2;
-                    params{ii}.rebalance_rate = 0;
-                    params{ii}.quad_u = true;
-%                     params{ii}.r_b = 0.02;
-                    params{ii}.quad_b = quad_b;
-                    params{ii}.name = sprintf('1A Quad util, b = %d, rho = %d', quad_b, rho);
-                end
-            end
-            
-            % 2A
-            for quad_b = quad_bs
-                for rho = rhos
-                    ii = ii + 1;
-                    params = [params {calibrations{1}}];
-                    params{ii} = params{1};
-                    params{ii}.calibration_vars = {'quad_b'};
-                    params{ii}.calibration_bounds = {b_bds};
-                    params{ii}.calibration_stats = {'totw'};
-                    params{ii}.calibration_targets = [scf.mean_totw];
-                    params{ii}.calibration_scales = [1];
-                    params{ii}.rho = rho;
+%             % 1A (with 2A r_b)
+%             for quad_b = quad_bs
+%                 for rho = rhos
+%                     ii = ii + 1;
+%                     params = [params {calibrations{1}}];
+%                     params{ii} = params{1};
+%                     params{ii}.calibration_vars = {'quad_b'};
+%                     params{ii}.calibration_bounds = {b_bds};
+%                     params{ii}.calibration_stats = {'totw'};
+%                     params{ii}.calibration_targets = [scf.mean_totw];
+%                     params{ii}.calibration_scales = [1];
+%                     params{ii}.rho = rho;
 %                     params{ii}.na = 2;
 %                     params{ii}.rebalance_rate = 0;
-                    params{ii}.quad_u = true;
-%                     params{ii}.r_b = 0.02;
-                    params{ii}.quad_b = quad_b;
-                    params{ii}.name = sprintf('2A Quad util, b = %d, rho = %d', quad_b, rho);
-                end
-            end
+%                     params{ii}.quad_u = true;
+% %                     params{ii}.r_b = 0.02;
+%                     params{ii}.quad_b = quad_b;
+%                     params{ii}.name = sprintf('1A Quad util, b = %d, rho = %d', quad_b, rho);
+%                 end
+%             end
+            
+            % 2A
+%             for quad_b = quad_bs
+%                 for rho = rhos
+%                     ii = ii + 1;
+%                     params = [params {calibrations{1}}];
+%                     params{ii} = params{1};
+%                     params{ii}.calibration_vars = {'quad_b'};
+%                     params{ii}.calibration_bounds = {b_bds};
+%                     params{ii}.calibration_stats = {'totw'};
+%                     params{ii}.calibration_targets = [scf.mean_totw];
+%                     params{ii}.calibration_scales = [1];
+%                     params{ii}.rho = rho;
+% %                     params{ii}.na = 2;
+% %                     params{ii}.rebalance_rate = 0;
+%                     params{ii}.quad_u = true;
+% %                     params{ii}.r_b = 0.02;
+%                     params{ii}.quad_b = quad_b;
+%                     params{ii}.name = sprintf('2A Quad util, b = %d, rho = %d', quad_b, rho);
+%                 end
+%             end
             
             
             
@@ -893,6 +893,7 @@ function [outparams, n] = overall_htm_target(param_opts)
         
         
 %         % 1A Baseline
+        
         for rho = [-0.004] %, -0.003, -0.002, -0.001, 0, 0.001, 0.002]
             ii = ii + 1;
             ii1A = ii;
@@ -907,6 +908,31 @@ function [outparams, n] = overall_htm_target(param_opts)
             params{ii}.ComputeMPCS_illiquid = false;
             params{ii}.rho = rho;
             params{ii}.name = sprintf('Baseline 1A, rho=%d', params{ii}.rho);
+        end
+        
+        for rho = rhos %[-0.004] %, -0.003, -0.002, -0.001, 0, 0.001, 0.002]
+            for quad_b = quad_bs
+                ii = ii + 1;
+                ii1A = ii;
+                params = [params {calibrations{1}}];
+                params{ii} = params{1};
+                params{ii}.calibration_vars = {'quad_b'};
+                params{ii}.calibration_bounds = {b_bds};
+                params{ii}.calibration_stats = {'totw'};
+                params{ii}.calibration_targets = [scf.mean_totw];
+                params{ii}.calibration_scales = [1];
+                params{ii}.OneAsset = true;
+                params{ii}.na = 2;
+                params{ii}.na_KFE = 2;
+                params{ii}.rebalance_rate = 0;
+                params{ii}.r_b = 0.0025;
+                params{ii}.r_a = params{ii}.r_b;
+                params{ii}.ComputeMPCS_illiquid = false;
+                params{ii}.rho = rho;
+                params{ii}.quad_u = true;
+                params{ii}.quad_b = quad_b;
+                params{ii}.name = sprintf('Baseline 1A w/ with quad_b=%d, rho=%d', quad_b, rho);
+            end
         end
             
             % Temptation robust to scaling of rho?
